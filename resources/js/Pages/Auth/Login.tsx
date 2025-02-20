964ae6e3131b6583d4f88261faa7cf1a8aab8d7e
rizwithnorizz <157877227+rizwithnorizz@users.noreply.github.com>
2025-02-21 02:18:05 +0800

    Favicon, Logo, App name, Main Redirectory

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -91,6 +91,9 @@ export default function Login({
                 </div>
 
                 <div className="mt-4 flex items-center justify-end">
+                    <Link href={route('register')} className="me-4 text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                        Register
+                    </Link>
                     {canResetPassword && (
                         <Link
                             href={route('password.request')}
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2803f18265afa051d8f8cbecd6fcc664d83b13b4',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -440,9 +440,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2803f18265afa051d8f8cbecd6fcc664d83b13b4',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
