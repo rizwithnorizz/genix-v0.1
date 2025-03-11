c2904e93553605334ca25183c335185a64118e18
rizwithnorizz <157877227+rizwithnorizz@users.noreply.github.com>
2025-03-11 23:26:22 +0800

    Update schedule.tsx

--- resources/js/Pages/GuestViewSchedule/schedule.tsx
+++ resources/js/Pages/GuestViewSchedule/schedule.tsx
@@ -11,6 +11,18 @@ import {
 import Layout from "@/Components/ui/layout";
 import AccountDropdown from "@/Components/ui/account-dropdown";
 
+const scheduleData = [
+  { time: "7:00-8:00", monday: "CC101", tuesday: "", wednesday: "CC102", thursday: "", friday: "", saturday: "" },
+  { time: "8:00-9:00", monday: "", tuesday: "", wednesday: "", thursday: "", friday: "", saturday: "" },
+  { time: "9:00-10:00", monday: "HCI", tuesday: "", wednesday: "", thursday: "", friday: "", saturday: "" },
+  { time: "11:00-12:00", monday: "", tuesday: "", wednesday: "", thursday: "", friday: "", saturday: "" },
+  { time: "1:00-2:00", monday: "", tuesday: "", wednesday: "", thursday: "", friday: "", saturday: "" },
+  { time: "2:00-3:00", monday: "", tuesday: "", wednesday: "", thursday: "", friday: "", saturday: "" },
+  { time: "4:00-5:00", monday: "", tuesday: "", wednesday: "", thursday: "", friday: "", saturday: "" },
+  { time: "5:00-6:00", monday: "", tuesday: "", wednesday: "", thursday: "", friday: "", saturday: "" },
+  { time: "7:00-8:00", monday: "", tuesday: "", wednesday: "", thursday: "", friday: "", saturday: "" },
+];
+
 const Schedule1 = () => {
   return (
     <Layout>
@@ -28,87 +40,17 @@ const Schedule1 = () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">7:00-8:00</TableCell>
-            <TableCell>CC101</TableCell>
-            <TableCell></TableCell>
-            <TableCell>CC102</TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium">8:00-9:00</TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium">9:00-10:00</TableCell>
-            <TableCell>HCI</TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium">11:00-12:00</TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium">1:00-2:00</TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium">2:00-3:00</TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium">4:00-5:00</TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium">5:00-6:00</TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium">7:00-8:00</TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-            <TableCell></TableCell>
-          </TableRow>
+          {scheduleData.map((schedule, index) => (
+            <TableRow key={index}>
+              <TableCell className="font-medium">{schedule.time}</TableCell>
+              <TableCell>{schedule.monday}</TableCell>
+              <TableCell>{schedule.tuesday}</TableCell>
+              <TableCell>{schedule.wednesday}</TableCell>
+              <TableCell>{schedule.thursday}</TableCell>
+              <TableCell>{schedule.friday}</TableCell>
+              <TableCell>{schedule.saturday}</TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </Layout>
