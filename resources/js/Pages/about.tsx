581063815f610d4a5f2e5d63feefde1ffee6e0d6
xjuuud01 <167752187+xjuuud01@users.noreply.github.com>
2025-03-27 01:17:56 +0800

    March 27 Jud

--- resources/js/Pages/about.tsx
+++ resources/js/Pages/about.tsx
@@ -1,14 +1,52 @@
 import Chart2 from '@/Components/ui/chart2';
 import Layout  from '@/Components/ui/layout';
 import Department from '@/Components/ui/department';
-import Pendings from '@/Components/ui/pendings';
-import News from '@/Components/ui/news';
+import { Card, CardFooter } from '@/Components/ui/card';
+import { Avatar, AvatarFallback, AvatarImage } from '@/Components/ui/avatar';
 
 
 const about = () => {
   return (
     <Layout>
-      <h1>about</h1>
+      <h1 className='pb-6'>About</h1>
+      <div className='pt-4'>
+        <Card className="bg-white p-4 rounded-2xl shadow-lg flex flex-row items-center">
+          <Avatar className="flex items-center justify-center h-40 w-40">
+            <AvatarImage className="h-full w-full" src="#" alt="CICT" />
+              <AvatarFallback className="text-center">Kyoto Logo</AvatarFallback>
+          </Avatar>
+          <div className="ml-4">
+            <h2 className="text-lg font-semibold">Kyoto</h2>
+              <p className="text-sm text-gray-500">Lorem ipsum dolor sit amet. Ea asperiores consequatur est similique molestiae qui accusantium consequatur et nulla blanditiis id reprehenderit voluptates aut saepe explicabo et quae voluptatibus.</p>
+            </div>
+        </Card>
+      </div>
+
+      <div className='pt-4'>
+        <Card className="bg-white p-4 rounded-2xl shadow-lg flex flex-row items-center">
+          <Avatar className="flex items-center justify-center h-40 w-40">
+            <AvatarImage className="h-full w-full" src="#" alt="CICT" />
+              <AvatarFallback className="text-center">Isaiah</AvatarFallback>
+          </Avatar>
+          <div className="ml-4">
+            <h2 className="text-lg font-semibold">Reven Jan Isaiah A. Aguilar</h2>
+              <p className="text-sm text-gray-500">Lorem ipsum dolor sit amet. Ea asperiores consequatur est similique molestiae qui accusantium consequatur et nulla blanditiis id reprehenderit voluptates aut saepe explicabo et quae voluptatibus.</p>
+            </div>
+        </Card>
+      </div>
+      
+      <div className='pt-4'>
+        <Card className="bg-white p-4 rounded-2xl shadow-lg flex flex-row items-center">
+          <Avatar className="flex items-center justify-center h-40 w-40">
+            <AvatarImage className="h-full w-full" src="#" alt="CICT" />
+              <AvatarFallback className="text-center">Jud</AvatarFallback>
+          </Avatar>
+          <div className="ml-4">
+            <h2 className="text-lg font-semibold">Judiel James A. Aristorenas</h2>
+              <p className="text-sm text-gray-500">Lorem ipsum dolor sit amet. Ea asperiores consequatur est similique molestiae qui accusantium consequatur et nulla blanditiis id reprehenderit voluptates aut saepe explicabo et quae voluptatibus.</p>
+            </div>
+        </Card>
+      </div>
 
     </Layout>
   );
--- resources/js/Pages/admin/help.tsx
+++ resources/js/Pages/admin/help.tsx
@@ -3,12 +3,39 @@ import Layout  from '@/Components/ui/layout';
 import Department from '@/Components/ui/department';
 import Pendings from '@/Components/ui/pendings';
 import News from '@/Components/ui/news';
+import { Card } from '@/Components/ui/card';
 
 
 export default function Help(){
   return (
     <Layout>
       <h1>Help</h1>
+
+      <Card className="bg-white p-4 rounded-2xl shadow-lg w-[15%] h-10 flex items-center justify-center mb-4">
+        <div>
+          <h2 className="text-lg font-semibold whitespace-nowrap">Sender Name here</h2>
+        </div>
+      </Card>
+
+      <Card className="bg-white p-4 rounded-2xl shadow-lg w-[80%] h-80 flex mb-4">
+        <div className="ml-4 ">
+        <h2 className="text-lg font-semibold break-words whitespace-normal">
+          Lorem ipsum dolor sit amet. Ea asperiores consequatur est similique molestiae qui accusantium consequatur et nulla blanditiis id reprehenderit voluptates aut saepe explicabo et quae voluptatibus.
+          Lorem ipsum dolor sit amet. Ea asperiores consequatur est similique molestiae qui accusantium consequatur et nulla blanditiis id reprehenderit voluptates aut saepe explicabo et quae voluptatibus.
+          Lorem ipsum dolor sit amet. Ea asperiores consequatur est similique molestiae qui accusantium consequatur et nulla blanditiis id reprehenderit voluptates aut saepe explicabo et quae voluptatibus.
+          Lorem ipsum dolor sit amet. Ea asperiores consequatur est similique molestiae qui accusantium consequatur et nulla blanditiis id reprehenderit voluptates aut saepe explicabo et quae voluptatibus.
+          </h2>
+        </div>
+      </Card>
+
+      <button className="bg-green-500 text-white p-2 rounded-lg mb-4">
+        Submit
+      </button>
+
+      <h2>Need help? Contact ITC</h2>
+      <h2>itc@ub.edu.ph</h2>
+      <h2>+639 999 999 9999</h2>
+      <h2>(043) 723-9999</h2>
 
     </Layout>
   );
