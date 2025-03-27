3e3b9ed0ddd05218393151efdbbfdc66c20d5efc
rizwithnorizz <157877227+rizwithnorizz@users.noreply.github.com>
2025-03-27 12:41:52 +0800

    Update superadmin_dashboard.tsx

--- resources/js/Pages/admin/superadmin_dashboard.tsx
+++ resources/js/Pages/admin/superadmin_dashboard.tsx
@@ -1,8 +1,5 @@
-import Chart2 from '@/Components/ui/chart2';
+
 import Layout  from '@/Components/ui/layout';
-import Department from '@/Components/ui/department';
-import Pendings from '@/Components/ui/pendings';
-import News from '@/Components/ui/news';
 import { useState } from 'react';
 import { Bell, type LucideIcon } from "lucide-react"
 
